9bf2febcf70de7228d383c170e7ccf6addec454a
Atauba Gideon <86924003+Gidii04@users.noreply.github.com>
2023-09-05 21:28:43 +0100

    add all files

--- script.js
+++ script.js
@@ -6,23 +6,37 @@ date.textContent = currentYear
 let menuBar = document.querySelector('.menu-bar')
 let closeBar = document.querySelector('.close-bar')
 let menuContainer = document.querySelector('.menu-container')
-
-menuBar.addEventListener('click', () => {
-  menuContainer.classList.add('show-menu-container')
-})
-closeBar.addEventListener('click', () => {
-  menuContainer.classList.remove('show-menu-container')
-})
-
 let navbar = document.querySelector('.navbar')
 
+menuBar.addEventListener('click', () => {
+  let menuHas = menuContainer.classList.contains('show-menu-container')
+  if (!menuHas) {
+    menuContainer.classList.add('show-menu-container')
+    menuContainer.style.top = window.scrollY + 'px'
+    document.body.style.overflow = 'hidden'
+    document.documentElement.style.overflow = 'hidden'
+    navbar.classList.remove('fixed-nav')
+  } else {
+    menuContainer.classList.remove('show-menu-container')
+    document.body.style.overflow = 'auto'
+    document.documentElement.style.overflow = 'auto'
+  }
+})
+
+closeBar.addEventListener('click', () => {
+  menuContainer.classList.remove('show-menu-container')
+  document.body.style.overflow = 'auto'
+  document.documentElement.style.overflow = 'auto'
+})
+
 window.addEventListener('scroll', () => {
+  console.log(window.scrollY)
+  let menuHas = menuContainer.classList.contains('show-menu-container')
+
   let pageHeight = window.pageYOffset
-  // console.log(pageHeight)
-  if (pageHeight > 466) {
+  if (pageHeight > 884 && !menuHas) {
     navbar.classList.add('fixed-nav')
   } else {
     navbar.classList.remove('fixed-nav')
   }
-  // console.log(window.pageYOffset)
 })
