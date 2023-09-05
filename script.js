c8021e9dcde19475c4a22768b26ca14067eee906
Atauba Gideon <86924003+Gidii04@users.noreply.github.com>
2023-09-05 20:10:59 +0100

    add all files

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-// let get = document.querySelector('.get-started')
+//year functionality
+let date = document.querySelector('#date')
+let currentYear = new Date().getFullYear()
+date.textContent = currentYear
 
-// get.addEventListener('click', (e) => {
-//   e.preventDefault()
-//   // console.log(e.getBoundingClientRect())
-//   let fig = get.getBoundingClientRect().width
-//   console.log(fig)
-//   // let e
-// })
+let menuBar = document.querySelector('.menu-bar')
+let closeBar = document.querySelector('.close-bar')
+let menuContainer = document.querySelector('.menu-container')
+
+menuBar.addEventListener('click', () => {
+  menuContainer.classList.add('show-menu-container')
+})
+closeBar.addEventListener('click', () => {
+  menuContainer.classList.remove('show-menu-container')
+})
+
+let navbar = document.querySelector('.navbar')
+
+window.addEventListener('scroll', () => {
+  let pageHeight = window.pageYOffset
+  // console.log(pageHeight)
+  if (pageHeight > 466) {
+    navbar.classList.add('fixed-nav')
+  } else {
+    navbar.classList.remove('fixed-nav')
+  }
+  // console.log(window.pageYOffset)
+})
